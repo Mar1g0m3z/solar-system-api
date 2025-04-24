78c74b53986d15e771b7fec3b7d482202489d0dc
Tamika Ross <tamika.ross.xu@gmail.com>
2025-04-24 16:35:37 -0500

    updating

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from .routes.planet_routes import planets_bp
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLAACHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
     db.init_app(app)
     migrate.init_app(app, db)
