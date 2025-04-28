c6175afa486ac9b7115f6a7148a7ac35db291b88
marisela <mariselaalejandragomez@gmail.com>
2025-04-28 12:00:43 -0700

    created get 1 planet by id, put, and delete

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planets import Planet
 from ..db import db
 # In Flask, url_prefix is an argument used when registering a blueprint. It adds a specified prefix to all routes defined within that blueprint.
@@ -25,6 +25,17 @@ def get_all_planets():
     return planets_response
 
 
+@planets_bp.get("/<id>")
+def get_one_planet(id):
+    planet = validate_planet(id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "number_of_moons": planet.number_of_moons,
+        "description": planet.description
+    }
+
+
 @planets_bp.post("")
 def create_planet():
     request_body = request.get_json()
@@ -47,6 +58,42 @@ def create_planet():
     return response, 201
 
 
+@planets_bp.put("/<id>")
+def update_planet(id):
+    planet = validate_planet(id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.number_of_moons = request_body["number_of_moons"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
+
+
+@planets_bp.delete("/<id>")
+def delete_planet(id):
+    planet = validate_planet(id)
+    db.session.delete(planet)
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
+
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except ValueError:
+        invalid_response = {"message": f"Planet id({id})is invalid."}
+        abort(make_response(invalid_response, 400))
+
+    query = db.select(Planet).where(Planet.id == id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        not_found = {"message": f"Planet with id({id}) not found."}
+        abort(make_response(not_found, 404))
+
+    return planet
 # def validate_planet(planet_id):
 #     try:
 #         planet_id = int(planet_id)
